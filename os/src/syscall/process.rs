4a95c2b0acd618ab2eac7c81c5395d1e5ba189c4
younggkid <zzshlcyy@sjtu.edu.cn>
2023-07-10 21:19:36 +0800

    feat:system call get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,9 +1,12 @@
 //! Process management syscalls
+use core::mem::size_of;
+
 use crate::{
     config::MAX_SYSCALL_NUM,
     task::{
-        change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus,
-    },
+        current_user_token, change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus,
+    }, mm::{translated_byte_buffer, VirtAddr},
+    timer::{get_time_us}
 };
 
 #[repr(C)]
@@ -43,7 +46,27 @@ pub fn sys_yield() -> isize {
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let vir_addr: VirtAddr= (_ts as usize).into();
+    let buffers = translated_byte_buffer(current_user_token(), 
+                                        vir_addr.0 as *const u8, 
+                                        size_of::<TimeVal>());
+    let us = get_time_us();
+    let time_val = TimeVal{
+        sec : us/1000000,
+        usec : us%1000000,
+    };
+    unsafe{
+        let time_val_bytes = core::slice::from_raw_parts((&time_val as *const TimeVal) as *const u8 , size_of::<TimeVal>());
+    
+    let mut idx = 0;
+    for buffer in buffers{
+        for i in 0..buffer.len(){
+            buffer[i] = time_val_bytes[idx];
+            idx += 1;
+        }
+    }
+}
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
@@ -52,8 +75,8 @@ pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
     trace!("kernel: sys_task_info NOT IMPLEMENTED YET!");
     -1
+    
 }
-
 // YOUR JOB: Implement mmap.
 pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
     trace!("kernel: sys_mmap NOT IMPLEMENTED YET!");
